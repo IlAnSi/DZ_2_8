9c41e51beebe151a0acf544add56336d027a6d20
IlAnSi <sia.muromez@gmail.com>
2022-05-09 02:10:36 +0500

    DZ 2/8 FILES 1 def ok

--- Cook_Book.py
+++ Cook_Book.py
@@ -1,34 +1,25 @@
-file = 'recipes.txt'
-cook_book = {}
-list_temp = []
 
-
-
-with open(file, encoding="utf-8") as file:
-
+def read_cook_book(file, cook_book_):
+    list_temp = []
     line1 = str(file.readline().strip())
     num2 = int(file.readline())
-    i =0
+    i = 0
     while i < num2:
         line = file.readline()
-        print(line)
-        list_line =line.split(' | ')
-        dict_ingr = {'ingredient_name': list_line[0], 'quantity': list_line[1], 'measure': list_line[2].strip()}
+        list_line = line.split(' | ')
+        dict_ingr = {'ingredient_name': list_line[0],
+                     'quantity': list_line[1],
+                     'measure': list_line[2].strip()}
         list_temp.append(dict_ingr)
-        i +=1
-    cook_book[line1] = list_temp
-    print(cook_book)
+        i += 1
+    cook_book_[line1] = list_temp
+    return cook_book_
 
-    for line in file:
-        line1 = str(file.readline().strip())
-        num2 = int(file.readline())
-        i = 0
-        while i < num2:
-            line = file.readline()
-            print(line)
-            list_line = line.split(' | ')
-            dict_ingr = {'ingredient_name': list_line[0], 'quantity': list_line[1], 'measure': list_line[2]}
-            list_temp.append(dict_ingr)
-            i += 1
-        cook_book[line1] = list_temp
-        print(cook_book)
+file_ = 'recipes.txt'
+cook_book = {}
+
+with open(file_, encoding="utf-8") as file_:
+    read_cook_book(file_, cook_book)
+    for line in file_:
+        read_cook_book(file_, cook_book)
+print(cook_book)
