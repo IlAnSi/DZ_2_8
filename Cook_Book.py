d09f46a4a4ec5d0775199b812abe51b01fdf4a40
IlAnSi <sia.muromez@gmail.com>
2022-05-09 00:43:27 +0500

    DZ 2/8 FILES 1 While

--- Cook_Book.py
+++ Cook_Book.py
@@ -8,10 +8,13 @@ with open(file, encoding="utf-8") as file:
 
     line1 = str(file.readline())
     num2 = int(file.readline())
-
-    for line in file.read().split('\n'):
+    i =0
+    while i < num2:
+        line = file.readline()
+        print(line)
         list_line =line.split(' | ')
         dict_ingr = {'ingredient_name': list_line[0], 'quantity': list_line[1], 'measure': list_line[2]}
         list_temp.append(dict_ingr)
+        i +=1
     cook_book[line1] = list_temp
     print(cook_book)
