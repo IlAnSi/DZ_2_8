5e3fd2d1cf49709fe6406aedb300388ac4f8fc30
IlAnSi <sia.muromez@gmail.com>
2022-05-09 01:38:10 +0500

    DZ 2/8 FILES 1 dict OK

--- Cook_Book.py
+++ Cook_Book.py
@@ -1,4 +1,4 @@
-file = 'recipes0.txt'
+file = 'recipes.txt'
 cook_book = {}
 list_temp = []
 
@@ -6,15 +6,29 @@ list_temp = []
 
 with open(file, encoding="utf-8") as file:
 
-    line1 = str(file.readline())
+    line1 = str(file.readline().strip())
     num2 = int(file.readline())
     i =0
     while i < num2:
         line = file.readline()
         print(line)
         list_line =line.split(' | ')
-        dict_ingr = {'ingredient_name': list_line[0], 'quantity': list_line[1], 'measure': list_line[2]}
+        dict_ingr = {'ingredient_name': list_line[0], 'quantity': list_line[1], 'measure': list_line[2].strip()}
         list_temp.append(dict_ingr)
         i +=1
     cook_book[line1] = list_temp
     print(cook_book)
+
+    for line in file:
+        line1 = str(file.readline().strip())
+        num2 = int(file.readline())
+        i = 0
+        while i < num2:
+            line = file.readline()
+            print(line)
+            list_line = line.split(' | ')
+            dict_ingr = {'ingredient_name': list_line[0], 'quantity': list_line[1], 'measure': list_line[2]}
+            list_temp.append(dict_ingr)
+            i += 1
+        cook_book[line1] = list_temp
+        print(cook_book)
